faafd76d1b9e81ef0b549aa21a18e054df7d989a
Steven <steventhon94@gmail.com>
2015-08-14 17:50:44 -0700

    Create Title-Case-a-Sentence.js

---
+++ Bonfire/Title-Case-a-Sentence.js
@@ -0,0 +1,9 @@
+function titleCase(str) {
+  str = str.toLowerCase().split(" ");
+  for (var i = 0; i < str.length; i++)
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].substring(1, str[i].length);
+  str = str.join(" ");
+  return str;
+}
+
+titleCase("I'm a little tea pot");
